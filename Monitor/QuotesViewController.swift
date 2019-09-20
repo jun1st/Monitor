3d7b6a922d436016c01786aee1bd207548dea795
fengde <jun1st.feng@gmail.com>
2019-09-20 10:21:56 +0800

    add Actions

--- Monitor/QuotesViewController.swift
+++ Monitor/QuotesViewController.swift
@@ -10,11 +10,38 @@ import Cocoa
 
 class QuotesViewController: NSViewController {
 
+    @IBOutlet var textLabel: NSTextField!
+    
+    let quotes = Quote.all
+    
+    var currentQuoteIndex: Int = 0 {
+        didSet {
+            updateQuote()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        currentQuoteIndex = 0
     }
     
+    @IBAction func previous(_ sender: NSButton) {
+        currentQuoteIndex = (currentQuoteIndex - 1 + quotes.count) % quotes.count
+    }
+    
+    @IBAction func next(_ sender: NSButton) {
+        currentQuoteIndex = (currentQuoteIndex + 1) % quotes.count
+    }
+    
+    @IBAction func quit(_ sender: NSButton) {
+        NSApplication.shared.terminate(sender)
+    }
+    
+    func updateQuote() {
+        textLabel.stringValue = String(describing: quotes[currentQuoteIndex])
+    }
 }
 
 extension QuotesViewController {
